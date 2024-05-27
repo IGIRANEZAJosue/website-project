400478a0d268b0d042384e5fc093671a2e706a8a
Josue Igiraneza <igiranezaj28@gmail.com>
2024-05-27 14:52:15 +0200

    Add Outfit font

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,11 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {},
+
+    fontFamily: {
+      'outfit': ['Outfit', 'sans-serif']
+    },
+
   },
   plugins: [],
 }
