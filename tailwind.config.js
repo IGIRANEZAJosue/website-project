41a221a896b2ab15f6bb57eaf7f2b3ea54b382ab
queen <nsengaqueen123@gmail.com>
2024-05-31 16:47:36 +0200

    Implementing the references section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,13 @@ module.exports = {
   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
   theme: {
     extend: {},
-
-    fontFamily: {
-      'outfit': ['Outfit', 'sans-serif']
+    colors: {
+      customYellow: "#FFE800",
     },
-
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"],
+    },
   },
   plugins: [],
-}
+};
 
