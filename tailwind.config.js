d9673a810bb5c655e2759bfd401f0166471618a1
Josue Igiraneza <igiranezaj28@gmail.com>
2024-06-05 20:05:13 +0200

    fix: fix the hero responsiveness on screens larger than "xl" with a custom breakpoint "cl"

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        customYellow: "#FFE800",
+   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+   theme: {
+      extend: {
+         colors: {
+            customYellow: "#FFE800",
+         },
+         fontFamily: {
+            outfit: ["Outfit", "sans-serif"],
+         },
+         screens: {
+            cl: "1340px",
+         },
       },
-      fontFamily: {
-        outfit: ["Outfit", "sans-serif"],
+   },
+   plugins: [
+      function ({ addUtilities }) {
+         addUtilities({
+            ".clip-bottom": {
+               "clip-path":
+                  "polygon(0 80%, 35% 95%, 100% 80%, 100% 100%, 0 100%)",
+            },
+         });
       },
-    },
-  },
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        ".clip-bottom": {
-          "clip-path": "polygon(0 80%, 35% 95%, 100% 80%, 100% 100%, 0 100%)",
-        },
-      });
-    },
-  ],
+   ],
 };
