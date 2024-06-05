54400ac60fbf14872354a3637a84b3526a746378
Josue Igiraneza <igiranezaj28@gmail.com>
2024-06-05 12:17:15 +0200

    fix: add background color to body

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
-  theme: {
-    extend: {},
+   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+   theme: {
+      extend: {
+         colors: {
+            fadeWhite: "#EFECEA",
+         },
+      },
 
-    fontFamily: {
-      'outfit': ['Outfit', 'sans-serif']
-    },
-
-  },
-  plugins: [],
-}
-
+      fontFamily: {
+         outfit: ["Outfit", "sans-serif"],
+      },
+   },
+   plugins: [],
+};
