f2bd54c73b962271fd46d023ac224b4be52e9428
Josue Igiraneza <igiranezaj28@gmail.com>
2024-06-05 22:59:39 +0200

    fix: fix the hero section responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", "./*.{html,js}"],
-  theme: {
-    extend: {
-      colors: {
-        customYellow: "#FFE800",
-        webGreen: "#00672E",
-        fadeWhite: "#EFECEA",
+   content: ["./src/**/*.{html,js}", "./*.{html,js}"],
+   theme: {
+      extend: {
+         colors: {
+            customYellow: "#FFE800",
+            webGreen: "#00672E",
+            fadeWhite: "#EFECEA",
+         },
+         fontFamily: {
+            outfit: ["Outfit", "sans-serif"],
+         },
+         screens: {
+            cl: "1340px",
+         },
       },
-      fontFamily: {
-        outfit: ["Outfit", "sans-serif"],
+   },
+   plugins: [
+      function ({ addUtilities }) {
+         addUtilities({
+            ".clip-bottom": {
+               "clip-path":
+                  "polygon(0 80%, 35% 95%, 100% 80%, 100% 100%, 0 100%)",
+            },
+         });
       },
-    },
-  },
-  plugins: [
-    function ({ addUtilities }) {
-      addUtilities({
-        ".clip-bottom": {
-          "clip-path": "polygon(0 80%, 35% 95%, 100% 80%, 100% 100%, 0 100%)",
-        },
-      });
-    },
-  ],
+   ],
 };
